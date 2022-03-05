c75024c797f02131445db1a19ac87eeb14c9e37c
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2022-03-05 20:54:54 +0200

    added API_KEY reference which i using local xcconfig file

---
+++ Clima/Models/WeatherManager.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Abdulrahman on 3/5/22.
+//
+
+import Foundation
+
+
+struct WeatherManager{
+    
+    static let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String
+    
+    let weatherURL = ""
+        
+    
+    
+}
